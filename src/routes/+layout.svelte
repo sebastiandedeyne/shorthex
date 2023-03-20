e5f45f9b8134f8c6cb9f1fbb952e9e7ab9f6993f
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2023-03-20 08:44:56 +0100

    Fix fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,8 +10,8 @@
     font-family: "Rubik";
     font-style: normal;
     font-weight: 400;
-    src: url("../fonts/rubik-v26-latin-regular.woff2") format("woff2"),
-      url("../fonts/rubik-v26-latin-regular.woff") format("woff");
+    src: url("/fonts/rubik-v26-latin-regular.woff2") format("woff2"),
+      url("/fonts/rubik-v26-latin-regular.woff") format("woff");
   }
 
   @font-face {
@@ -19,8 +19,8 @@
     font-family: "Rubik";
     font-style: normal;
     font-weight: 700;
-    src: url("../fonts/rubik-v26-latin-700.woff2") format("woff2"),
-      url("../fonts/rubik-v26-latin-700.woff") format("woff");
+    src: url("/fonts/rubik-v26-latin-700.woff2") format("woff2"),
+      url("/fonts/rubik-v26-latin-700.woff") format("woff");
   }
 
   @font-face {
@@ -28,8 +28,8 @@
     font-family: "Space Mono";
     font-style: normal;
     font-weight: 400;
-    src: url("../fonts/space-mono-v12-latin-regular.woff2") format("woff2"),
-      url("../fonts/space-mono-v12-latin-regular.woff") format("woff");
+    src: url("/fonts/space-mono-v12-latin-regular.woff2") format("woff2"),
+      url("/fonts/space-mono-v12-latin-regular.woff") format("woff");
   }
 
   :global(html) {
